d9cd19eb799f703a998128dde0710df1ecd7f9d2
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2024-09-01 23:08:51 -0300

    editando resposta para usuario

--- Routes/teste.js
+++ Routes/teste.js
@@ -12,10 +12,10 @@ RotaTeste.get("/teste", async (req, res)=>{
         console.log("solicitando dados")
         const resposta = await db.query(`SELECT * FROM usuarios`);
         console.log("enviando dados ao usuario")
-        res.sendStatus(200).send(resposta);
+        res.status(200).send(resposta);
 
     }catch(e){
-        res.sendStatus(404).send(`Erro ao tentar se conectar: ${e}`);
+        res.status(404).send(`Erro ao tentar se conectar: ${e}`);
     }finally{
         console.log("finalizando requisição")
         db.end();
