8847fcb62c4e2e8ea919effdd579e2dcb9316d10
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2024-09-07 17:03:34 -0300

    simplificando a consulta

--- Controllers/abrirLinkController.js
+++ Controllers/abrirLinkController.js
@@ -3,27 +3,29 @@ import db from "../Banco/index.js";
 export default async function abrirLinkController(req, res, next){
     const {params} = req;
     const {shortUrl} = params;
-    const localizarLink = `
-        SELECT url 
-        FROM links
-        WHERE links.shorturl = $1
-        LIMIT 1
+
+    const comandoQuery = `
+        UPDATE links
+        SET qtvisitas = qtvisitas + 1
+        WHERE shorturl = $1
+        RETURNING url
     `;
+
     const valores = [shortUrl];
     let respostaBanco;
     let link; 
 
 
     try{        
-        respostaBanco = await db.query(localizarLink, valores);
+        respostaBanco = await db.query(comandoQuery, valores);
         link = respostaBanco.rows[0]?.url;
+        
         if(link === undefined){
             return res.status(404).send("Url não encontrada");
         }
-
         res.redirect(link)
     
     }catch(e){
-        res.status(400).send(`Erro: ${e}`)
+        res.status(400).send(`Erro: ${e}`);
     }
 }
